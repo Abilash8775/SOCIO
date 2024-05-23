df5f5e74008328d7098bf8541d0c73d73c72bacb
ABILASH <abilash8861547610@gmail.com>
2024-05-23 09:51:23 +0530

    socio

--- social_book/settings.py
+++ social_book/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kvuk$z)o67koi#6v!vhxno#q4&sq!(jw4rsp7x!*8mag!-*zvz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 # Application definition
     
